586bcd061fa14207526732b2a3b6a310a5fb8925
cabrinilm <luca_cabrinimm@hotmail.com>
2025-03-06 15:06:19 +0000

    change password done

--- mapReactNative/App.js
+++ mapReactNative/App.js
@@ -1,27 +1,27 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import React, { useEffect } from 'react';
-import { I18nextProvider } from 'react-i18next';  
-import i18n from './src/services/i18n';  
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import React, { useEffect } from "react";
+import { I18nextProvider } from "react-i18next";
+import i18n from "./src/services/i18n";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 // SCREEN IMPORTS
-import HomeScreen from './components/screens/HomeScreen';
-import LoginScreen from './components/screens/LoginScreen';
-import SignUpScreen from './components/screens/SignUpScreen';
-import ProfileScreen from './components/screens/ProfileScreen';
-import FavoritesScreen from './components/screens/FavoritesScreen';
-import SettingsScreen from './components/screens/Settings/SettingsScreen';
-import CoffeeProfileScreen from './components/screens/CoffeeProfileScreen';
-import NotificationsScreen from './components/screens/Settings/NotificationsScreen';
-import LanguageSelectionScreen from './components/screens/Settings/LanguageSelectionScreen';
-import PrivacyScreen from './components/screens/Settings/PrivacyScreen';
+import HomeScreen from "./components/screens/HomeScreen";
+import LoginScreen from "./components/screens/LoginScreen";
+import SignUpScreen from "./components/screens/SignUpScreen";
+import ProfileScreen from "./components/screens/ProfileScreen";
+import FavoritesScreen from "./components/screens/FavoritesScreen";
+import SettingsScreen from "./components/screens/Settings/SettingsScreen";
+import CoffeeProfileScreen from "./components/screens/CoffeeProfileScreen";
+import NotificationsScreen from "./components/screens/Settings/NotificationsScreen";
+import LanguageSelectionScreen from "./components/screens/Settings/LanguageSelectionScreen";
+import PrivacyScreen from "./components/screens/Settings/PrivacyScreen";
+import ChangePasswordScreen from "./components/screens/Settings/ChangePasswordScreen";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-   
     <I18nextProvider i18n={i18n}>
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Home">
@@ -36,13 +36,42 @@ export default function App() {
             options={{ headerShown: false }}
           />
           <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
-          <Stack.Screen name="Profile" component={ProfileScreen} options={{ title: 'Profile' }} />
-          <Stack.Screen name="Favorites" component={FavoritesScreen} options={{ title: 'Favorites' }} />
-          <Stack.Screen name="Settings" component={SettingsScreen} options={{ title: 'Settings' }} />
-          <Stack.Screen name="CoffeeProfile" component={CoffeeProfileScreen} options={{ title: 'Coffee Profile' }}/>
-          <Stack.Screen name="Notifications" component={NotificationsScreen} options={{title: 'Notifications'}} />
-          <Stack.Screen name="LanguageSelection" component={LanguageSelectionScreen} options={{ title: 'Select Language' }} />
-          <Stack.Screen name="PrivacyScreen" component={PrivacyScreen}/>
+          <Stack.Screen
+            name="Profile"
+            component={ProfileScreen}
+            options={{ title: "Profile" }}
+          />
+          <Stack.Screen
+            name="Favorites"
+            component={FavoritesScreen}
+            options={{ title: "Favorites" }}
+          />
+          <Stack.Screen
+            name="Settings"
+            component={SettingsScreen}
+            options={{ title: "Settings" }}
+          />
+          <Stack.Screen
+            name="CoffeeProfile"
+            component={CoffeeProfileScreen}
+            options={{ title: "Coffee Profile" }}
+          />
+          <Stack.Screen
+            name="Notifications"
+            component={NotificationsScreen}
+            options={{ title: "Notifications" }}
+          />
+          <Stack.Screen
+            name="LanguageSelection"
+            component={LanguageSelectionScreen}
+            options={{ title: "Select Language" }}
+          />
+          <Stack.Screen name="PrivacyScreen" component={PrivacyScreen} />
+          <Stack.Screen
+            name="ChangePassword"
+            component={ChangePasswordScreen}
+            options={{ title: "Change Password" }} 
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </I18nextProvider>
